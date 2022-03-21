992fc5da4b1c9d013e92d8a5de19d53122baca47
Alan Thomas Shaji <50659499+Captain-shaji-stark@users.noreply.github.com>
2022-03-21 15:33:44 +0530

    Chat homepage Ui

--- HLF Chat/lib/chatPage.dart
+++ HLF Chat/lib/chatPage.dart
@@ -31,6 +31,17 @@ class _HomeState extends State<Home> {
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            ChatListHeader(),
+            ChatListHeader(),
+            ChatListHeader(),
+            ChatListHeader(),
+          ],
+        ),
+      ),
       bottomNavigationBar: Container(
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         height: 60,
@@ -86,6 +97,55 @@ class _HomeState extends State<Home> {
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatListHeader extends StatelessWidget {
+  const ChatListHeader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Align(
+        alignment: Alignment.center,
+        child: Container(
+          padding: EdgeInsets.all(14),
+          height: 75,
+          width: Get.width * 0.94,
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 230, 230, 230),
+            borderRadius: BorderRadius.circular(18),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/images/avatar.jpg'),
+                radius: 25,
+              ),
+              Spacer(flex: 1),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Jaime Allen'),
+                    SizedBox(height: 4),
+                    Text('You: Okay'),
+                  ],
+                ),
+              ),
+              Spacer(flex: 16),
+              Text('Time')
+            ],
+          ),
         ),
       ),
     );
