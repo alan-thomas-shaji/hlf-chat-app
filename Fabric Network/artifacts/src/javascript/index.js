3ca446a0d55929ea6ce155a5da7cfe6785f89d6a
Navaneeth Krishna E S <naveekurosaki@gmail.com>
2022-05-14 19:05:31 +0530

    Added fabric network files

---
+++ Fabric Network/artifacts/src/javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const MessageContract = require('./lib/messageContract');
+
+module.exports.MessageContract = MessageContract;
+module.exports.contracts = [MessageContract];
