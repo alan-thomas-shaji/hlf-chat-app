e8bcc058f8548689cde9c54db8972aba4387c63c
Navaneeth Krishna E S <naveekurosaki@gmail.com>
2022-05-25 15:49:10 +0530

    Added forward message socket event

--- Node/index.js
+++ Node/index.js
@@ -1,12 +1,38 @@
 require("dotenv").config();
 const app = require("express")();
 const axios = require("axios");
+const Logger = require("nodemon/lib/utils/log");
 const http = require("http").createServer(app);
 const { uploadImage } = require("./aws-upload");
 const io = require("socket.io")(http, {
   cors: { origin: "*:*" },
   serveClient: false,
 });
+
+const createMessage = (data) => {
+  console.log(data);
+  axios
+    .post(process.env.REST_API_URL + "messages/" + chatID, {
+      sender: data.senderChatID,
+      receiver: data.receiverChatID,
+      content: data.message,
+      timestamp: data.timestamp,
+      isMedia: false,
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  //Send message to only that particular room
+  io.emit("receive_message", {
+    content: data.message,
+    senderChatID: data.senderChatID,
+    receiverChatID: data.receiverChatID,
+  });
+};
 
 app.get("/", (req, res) => {
   res.send("Server running.");
@@ -55,29 +81,19 @@ io.on("connection", (socket) => {
   });
 
   //Send message to only a particular user
-  socket.on("message", (data) => {
-    console.log(data);
+  socket.on("message", createMessage(data));
+
+  socket.on("forward", (data) => {
     axios
-      .post(process.env.REST_API_URL + "messages/" + chatID, {
-        sender: data.senderChatID,
-        receiver: data.receiverChatID,
-        content: data.message,
-        timestamp: data.timestamp,
-        isMedia: false,
-      })
-      .then((response) => {
-        console.log(response);
+      .get(process.env.REST_API_URL + "message/" + data.messageID)
+      .then((resMessage) => {
+        resMessage.forwardCount += 1;
+        //some more code that is unaffecting to the chat to be added here.
+        createMessage(data);
       })
       .catch((error) => {
         console.log(error);
       });
-
-    //Send message to only that particular room
-    io.emit("receive_message", {
-      content: data.message,
-      senderChatID: data.senderChatID,
-      receiverChatID: data.receiverChatID,
-    });
   });
 });
 
