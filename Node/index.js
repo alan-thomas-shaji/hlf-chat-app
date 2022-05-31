fe1fa5e1a85ba4145f3da3118add717f872a3f3d
Navaneeth Krishna E S <naveekurosaki@gmail.com>
2022-05-31 18:27:21 +0530

    Fixed the issue with media socket event

--- Node/index.js
+++ Node/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("express")();
 const http = require("http").createServer(app);
-// const { uploadImage } = require("./aws-upload");
+const { uploadImage } = require("./aws-upload");
 const {
   createMessage,
   getMessage,
@@ -11,6 +11,31 @@ const io = require("socket.io")(http, {
   cors: { origin: "*:*" },
   serveClient: false,
 });
+
+// const msg = {
+//   sender: "V1StGXR8_Z5jdHi6B-myc",
+//   receiver: "V1StGXR8_Z5jdHi6B-myX",
+//   content: "Greetings...",
+//   timestamp: Date.now(),
+//   deviceMAC: "20:a2:8c:10:4f",
+//   isMedia: false,
+// };
+
+// createMessage(msg)
+//   .then((response) => console.log(response.data))
+//   .catch((error) => console.error(error));
+
+// getMessage("6290cecbaf242394766b7167")
+//   .then((response) => {
+//     let message = response.data;
+//     message.forwardCount += 1;
+//     patchMessage("6290cecbaf242394766b7167", {
+//       forwardCount: message.forwardCount,
+//     })
+//       .then((response) => console.log(message))
+//       .catch((error) => console.error(error));
+//   })
+//   .catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
   res.send("Server running.");
@@ -41,21 +66,21 @@ io.on("connection", (socket) => {
       .catch((error) => console.error(error));
   });
 
-  // socket.on("media", (data) => {
-  //   const mediaUrl = uploadImage(Buffer(data.image));
-  //   const mediaMessage = {
-  //     sender: data.senderChatID,
-  //     receiver: data.receiverChatID,
-  //     content: mediaUrl,
-  //     deviceMAC: data.deviceMAC,
-  //     timestamp: data.timestamp,
-  //     isMedia: false,
-  //   };
+  socket.on("media", (data) => {
+    const mediaUrl = uploadImage(Buffer(data.image));
+    const mediaMessage = {
+      sender: data.senderChatID,
+      receiver: data.receiverChatID,
+      content: mediaUrl,
+      deviceMAC: data.deviceMAC,
+      timestamp: data.timestamp,
+      isMedia: false,
+    };
 
-  //   createMessage(mediaMessage)
-  //     .then((response) => io.emit("receive_message", response.data))
-  //     .catch((error) => console.error(error));
-  // });
+    createMessage(mediaMessage)
+      .then((response) => io.emit("receive_message", response.data))
+      .catch((error) => console.error(error));
+  });
 
   socket.on("forward", (data) => {
     getMessage(data.messageID)
