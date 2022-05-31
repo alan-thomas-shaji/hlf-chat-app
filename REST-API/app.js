36afc40a947f196dd3a5cb7580b665f4e638a114
Navaneeth Krishna E S <naveekurosaki@gmail.com>
2022-06-01 01:30:26 +0530

    Added api invocation to blockchain client on create messasge route and restructured the res.send statements

--- REST-API/app.js
+++ REST-API/app.js
@@ -9,7 +9,7 @@ const { createMessageBlock } = require("./API/lib/message");
 const { parse, stringify, toJSON, fromJSON } = require("flatted");
 const { msgSchema, userSchema, chatIDSchema } = require("./schemas/schema.js");
 const { nanoid } = require("nanoid");
-const { getJWT, updateJWT } = require("./API/tokenService");
+const { getJWT, updateJWT, refreshJWT } = require("./API/tokenService");
 const axiosClient = require("./API/apiClient");
 const app = express();
 
@@ -19,8 +19,6 @@ if (typeof localStorage === "undefined" || localStorage === null) {
 }
 
 logger.level = "debug";
-
-console.log(getJWT());
 
 //Connecting MongoDB
 if (Boolean(process.env.PRODUCTION)) {
@@ -88,7 +86,9 @@ app.get("/users/:userId", (req, res) => {
     if (err) {
       res.send(err);
     }
-    res.send(foundUser);
+    if (!err) {
+      res.send(foundUser);
+    }
   });
 });
 
@@ -163,13 +163,14 @@ app.get("/messages/all/:userId", (req, res) => {
       if (err) {
         res.send(err);
       }
-      res.send(foundMessages);
+      if (!err) {
+        res.send(foundMessages);
+      }
     }
   );
 });
 
 app.post("/messages/create", (req, res) => {
-  console.log(req.body);
   const newMessage = new Message({
     sender: req.body.sender,
     receiver: req.body.receiver,
@@ -177,15 +178,102 @@ app.post("/messages/create", (req, res) => {
     deviceMAC: req.body.deviceMAC,
     timestamp: req.body.timestamp,
     isMedia: req.body.isMedia,
+    isForwarded: req.body.isForwarded,
   });
+  //Initialising message object
+
+  if (req.body.isMedia === true) {
+    token = getJWT();
+
+    if (token === "") {
+      const refreshUser = {
+        username: "admin",
+        orgName: "Org1",
+      };
+      axiosClient
+        .post("/users", refreshUser)
+        .then((response) => {
+          console.log("Interceptor token recived: " + response.data.token);
+          updateJWT(response.data.token);
+          token = getJWT();
+        })
+        .catch((error) =>
+          console.error("Interceptor JWT refresh error: " + error)
+        );
+    }
+
+    console.log("Invoking API with token: " + token);
+
+    const header = { Authorization: `Bearer ${token}` };
+
+    const id = req.body.content.split("/")[3].split(".")[0];
+
+    if (req.body.isForwarded === true) {
+      const forwardInvokeCC = {
+        fcn: "forwardMessage",
+        peers: ["peer0.org1.example.com"],
+        chaincodeName: "messagecontract",
+        channelName: "mychannel",
+        args: [
+          String(id),
+          String(req.body.sender),
+          String(req.body.receiver),
+          String(req.body.timestamp),
+        ],
+      };
+
+      createMessageBlock(forwardInvokeCC, header)
+        .then((response) => {
+          // console.log("token: " + getJWT());
+          console.info(response.data);
+        })
+        .catch((error) => {
+          if (error.response.status === 401) {
+            console.log("token: " + getJWT());
+            error.config.headers["Authorization"] = "Bearer " + getJWT();
+            axiosClient.request(error.config);
+          }
+        });
+    } else {
+      const invokeCC = {
+        fcn: "createMessage",
+        peers: ["peer0.org1.example.com"],
+        chaincodeName: "messagecontract",
+        channelName: "mychannel",
+        args: [
+          String(id),
+          String(req.body.sender),
+          String(req.body.deviceMAC),
+          String(req.body.receiver),
+          String(req.body.content),
+          String(req.body.timestamp),
+        ],
+      };
+
+      createMessageBlock(invokeCC, header)
+        .then((response) => {
+          // console.log("token: " + getJWT());
+          console.info(response.data);
+        })
+        .catch((error) => {
+          if (error.response.status === 401) {
+            console.log("token: " + getJWT());
+            error.config.headers["Authorization"] = "Bearer " + getJWT();
+            axiosClient.request(error.config);
+          }
+        });
+    }
+  }
+
   newMessage.save((err) => {
     if (err) {
       res.send(err);
     }
     if (!err) {
-      res.send(JSON.stringify(newMessage));
+      res.send(newMessage);
     }
   });
+  //Message saved to MongoDB
 });
 
 app.patch("/messages/update/:messageId", (req, res) => {
@@ -222,42 +310,6 @@ app.post("/messages/:chatId", (req, res) => {
     newMessage.save();
 
     // logger.info("Message ID: " + newMessage._id);
-
-    token = getJWT();
-
-    const header = { Authorization: `Bearer ${token}` };
-
-    const blockData = {
-      fcn: "createMessage",
-      peers: ["peer0.org1.example.com"],
-      chaincodeName: "messagecontract",
-      channelName: "mychannel",
-      args: [
-        String(newMessage._id),
-        String(req.body.sender),
-        String(req.body.deviceMAC),
-        String(req.body.receiver),
-        String(req.body.content),
-        String(req.body.timestamp),
-      ],
-    };
-
-    createMessageBlock(blockData, header)
-      .then((response) => {
-        console.log("token: " + getJWT());
-        // console.info(response.data);
-      })
-      .catch((error) => {
-        if (error.response.status === 401) {
-          console.log("token: " + getJWT());
-          error.config.headers["Authorization"] = "Bearer " + getJWT();
-          axiosClient.request(error.config);
-        }
-      });
-
-    res.send(
-      `New message: ${newMessage} added to chatID: ${req.params.chatId}`
-    );
   });
 });
 
